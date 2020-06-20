52403fa68ba7826c890dba02d54bf32839412886
james-n-007 <58572502+james-n-007@users.noreply.github.com>
2020-06-20 14:14:15 +0530

    Create raw_api_response.js

---
+++ raw_api_response.js
@@ -0,0 +1,25 @@
+{
+  "responseId": "9ede31cd-4433-4adc-91bb-899dbb86a3a8-83ffff32",
+  "queryResult": {
+    "queryText": "hi",
+    "action": "input.welcome",
+    "parameters": {},
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Hi! How are you doing?",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Hi! How are you doing?"
+          ]
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/test-1-nq9i/agent/intents/c4ed249c-b9d6-45f7-b39f-ff190ff7b1fb",
+      "displayName": "Default Welcome Intent"
+    },
+    "intentDetectionConfidence": 1,
+    "languageCode": "en"
+  }
+}
